2705b1f4851bd8f8a05d51fffdeeb6886cfda5d9
Dwihadi Bima Pangestu <94498243+bima29@users.noreply.github.com>
2024-12-10 13:35:04 +0700

    up
    up

--- application/views/admin/partials/footer.php
+++ application/views/admin/partials/footer.php
@@ -1,4 +1,3 @@
- <!-- /.content-wrapper -->
  <footer class="main-footer">
      <strong>Copyright &copy; 2024 <a href="http://btsstoreindonesia.com/">BTSGames</a>.</strong>
      All rights reserved.
@@ -7,62 +6,42 @@
      </div>
  </footer>
 
-
- <!-- Control Sidebar -->
  <aside class="control-sidebar control-sidebar-dark">
-     <!-- Control sidebar content goes here -->
  </aside>
- <!-- /.control-sidebar -->
  </div>
- <!-- ./wrapper -->
 
- <!-- jQuery -->
  <script src="<?= base_url('assets/dashboard'); ?>/plugins/jquery/jquery.min.js"></script>
 
- <!-- jQuery UI 1.11.4 -->
  <script src="<?= base_url('assets/dashboard'); ?>/plugins/jquery-ui/jquery-ui.min.js"></script>
 
- <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
  <script>
      $.widget.bridge('uibutton', $.ui.button)
  </script>
 
- <!-- Bootstrap 4 -->
  <script src="<?= base_url('assets/dashboard'); ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 
- <!-- ChartJS -->
  <script src="<?= base_url('assets/dashboard'); ?>/plugins/chart.js/Chart.min.js"></script>
 
- <!-- Sparkline -->
  <script src="<?= base_url('assets/dashboard'); ?>/plugins/sparklines/sparkline.js"></script>
 
- <!-- JQVMap -->
  <script src="<?= base_url('assets/dashboard'); ?>/plugins/jqvmap/jquery.vmap.min.js"></script>
  <script src="<?= base_url('assets/dashboard'); ?>/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
 
- <!-- jQuery Knob Chart -->
  <script src="<?= base_url('assets/dashboard'); ?>/plugins/jquery-knob/jquery.knob.min.js"></script>
 
- <!-- daterangepicker -->
  <script src="<?= base_url('assets/dashboard'); ?>/plugins/moment/moment.min.js"></script>
  <script src="<?= base_url('assets/dashboard'); ?>/plugins/daterangepicker/daterangepicker.js"></script>
 
- <!-- Tempusdominus Bootstrap 4 -->
  <script src="<?= base_url('assets/dashboard'); ?>/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
 
- <!-- Summernote -->
  <script src="<?= base_url('assets/dashboard'); ?>/plugins/summernote/summernote-bs4.min.js"></script>
 
- <!-- overlayScrollbars -->
  <script src="<?= base_url('assets/dashboard'); ?>/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 
- <!-- AdminLTE App -->
  <script src="<?= base_url('assets/dashboard'); ?>/dist/js/adminlte.js"></script>
 
- <!-- AdminLTE for demo purposes -->
  <script src="<?= base_url('assets/dashboard'); ?>/dist/js/pages/dashboard.js"></script>
 
- <!-- DataTables -->
  <script src="<?= base_url('assets/dashboard'); ?>/plugins/datatables/jquery.dataTables.min.js"></script>
  <script src="<?= base_url('assets/dashboard'); ?>/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
 
@@ -107,10 +86,6 @@
          $(".dataTables_filter").addClass("search-container");
      });
  </script>
-
-
-
-
  </body>
 
  </html>
