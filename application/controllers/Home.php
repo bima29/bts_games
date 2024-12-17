203f0b7f05ce5e4f8861dd21e0776b56723bf0c0
Dwihadi Bima Pangestu <94498243+bima29@users.noreply.github.com>
2024-12-17 14:06:40 +0700

    upd
    upd

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -59,6 +59,35 @@ class Home extends CI_Controller
         $this->load->view('Partials/footer');
     }
 
+    public function Checkout1($id)
+{
+    if (!$this->session->userdata('user_id')) {
+        redirect(base_url('auth'));
+    }
+
+    // Ambil data game berdasarkan ID
+    $data['game'] = $this->db->get_where('games', ['id' => $id])->row();
+
+    if (!$data['game']) {
+        show_404();
+    }
+
+    // Ambil data price_list berdasarkan game_name
+    $data['price_list'] = $this->db->get_where('price_list', ['product_name' => $data['game']->game_name])->result();
+
+    // Ambil unit unik dari price_list
+    $this->db->distinct();
+    $this->db->select('unit');
+    $this->db->where('product_name', $data['game']->game_name);
+    $data['units'] = $this->db->get('price_list')->result();
+
+    $this->load->view('Partials/Fitur/header', $data);
+    $this->load->view('Partials/Fitur/Checkout/content1', $data);
+    $this->load->view('Partials/Fitur/footer');
+}
+
+
+
     public function logout()
     {
         $this->session->sess_destroy();
